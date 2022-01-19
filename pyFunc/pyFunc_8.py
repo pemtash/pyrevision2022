1ecb4dae3f0bfa169e372dee3977cab9dbc73c8f
pemtash <70712789+pemtash@users.noreply.github.com>
2022-01-19 11:29:19 +0530

    Add files via upload

--- pyFunc/pyFunc_8.py
+++ pyFunc/pyFunc_8.py
@@ -7,8 +7,16 @@ def mySquare(num):
 
 # mySquare(10)
 
-# def myCube(n):
-#    return n * n * n
+def myCube(n):
+    return n * n * n
+
+def iterFactorial(num):
+    result = 1
+    for item in range(1, num + 1):
+        result = result * item
+
+    return result
+
 
 def WrapperFunction(funcAsParam, num):
     return funcAsParam(num)
@@ -18,3 +26,10 @@ result = WrapperFunction(mySquare, 5)
 print('result:', result)
 
 # Functions are also first class objects in python
+# closures
+# decorators
+
+lstFunc = [mySquare, myCube, iterFactorial]
+
+for item in lstFunc:
+    print('result: ', item(5))
